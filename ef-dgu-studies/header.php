01345e31db6e259382cd3a8178def7521476c20c
islam-404 <55444050+islam-404@users.noreply.github.com>
2023-01-24 13:05:42 +0300

    rr
    ff

---
+++ ef-dgu-studies/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#000">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>ef-dgu-studies</title>
+</head>
+<body>
+    
+    <nav class="navbar">
+        <div class="container">
+            <div class="navbar-container">
+                <div class="navbar-block">
+                <div class="navbar__logo">
+                <div class="navbar__logo-block">
+                    <a href="#"><img src="img/logo.png" alt=""></a>
+                </div>
+            </div>
+                <div class="navbar-text">
+                    <p class="navbar-text__title">Экономический <br> факультет</p>
+                    <p class="navbar-text__subtitle">Дагестанский государственный <br> университет</p>
+                </div>
+            </div>
+            <div class="navbar__right-block">Курсы Экономического факультета</div>
+            </div>
+        </div>
+        <!-- /.cotainer -->
+    </nav>
+
+
+    <div class="main">
+        <div class="container">
+            <div class="main-block">
+                <div class="main-info">
+                    <div class="main-info__fullName">ФИО: <br> Магомедов Гебек</div>
+                    <div class="main-info__group">Студент Экономического факультета</div>
+                </div>
+                <div class="main-navigation">
+                    <div class="main-navigation__buttons">
+                        <div class="navigation__button">
+                    <a href="#" class="">Образовательные курсы</a>
+                </div>
